836f54fde53e5b85777522ff41b4c808dff6a715
Palak <palak0758.be23@chitkara.edu.in>
2024-02-27 09:38:27 +0530

    My code in C

--- hello.c
+++ hello.c
@@ -1,5 +1,11 @@
-#include <stdio.h>
-void main ()
-{
-    printf("Hello world!!");
+#include<stdio.h>
+int main(){
+    int a,b,c,d;
+    printf("Enter the value of a,b , c and d:\n");
+    scanf("%d%d%d%d",&a,&b,&c,&d);
+    printf("%d\n",a+b);
+    printf("%d\n",a-b);
+    printf("%d\n",a*b);
+    printf("%d\n",a/b);
+
 }
